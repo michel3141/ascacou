d6f9ea5fa4f80161fd9f3c082333af052279b114
MicheL <michel@clicdev.itneo.fr>
2023-05-18 00:08:09 +0200

    panel background css

--- src/features/app/Bar.jsx
+++ src/features/app/Bar.jsx
@@ -1,14 +1,17 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, lazy, Suspense } from 'react';
+import { SkipPrevious, Replay, Help, Menu as MenuIcn } from '@mui/icons-material';
+
+import './Bar.scss';
+
 import { AppBar } from '@mui/material';
 import { RULES, VICTORY, CONFIG } from '/app/constants/drawers';
 import titre from '/assets/img/titre-t.png';
 import { useAppSlice, useAscacouSlice } from '/app/slices';
-import Config from '/features/params/Config';
-import Menu from './Menu';
-import Regles from './Regles';
-import Victoire from './Victoire';
 
-import { SkipPrevious, Replay, Help, Menu as MenuIcn } from '@mui/icons-material';
+const Config = lazy(() => import('/features/params/Config'));
+const Menu = lazy(() => import('./Menu'));
+const Regles = lazy(() => import('./Regles'));
+const Victoire = lazy(() => import('./Victoire'));
 
 const Bar = () => {
   const { useDrawer, toggleDrawer } = useAppSlice();
@@ -40,33 +43,35 @@ const Bar = () => {
       position='static'
       color='transparent'
     >
-      <Menu
-        actions={actions}
-        drawers={[
-          {
-            title: 'Règles',
-            lbl: <Help />,
-            action: <Regles />,
-            ...visibility(RULES),
-          },
-          {
-            title: 'Nouvelle partie',
-            lbl: <MenuIcn />,
-            action: <Config />,
-            ...visibility(CONFIG),
-          },
-          {
-            action: <Victoire />,
-            ...visibility(VICTORY),
-          },
-        ]}
-        titre={
-          <img
-            src={titre}
-            onMouseDown={(e) => e.preventDefault()}
-          />
-        }
-      />
+      <Suspense fallback={'...'}>
+        <Menu
+          actions={actions}
+          drawers={[
+            {
+              title: 'Règles',
+              lbl: <Help />,
+              action: <Regles />,
+              ...visibility(RULES),
+            },
+            {
+              title: 'Nouvelle partie',
+              lbl: <MenuIcn />,
+              action: <Config />,
+              ...visibility(CONFIG),
+            },
+            {
+              action: <Victoire />,
+              ...visibility(VICTORY),
+            },
+          ]}
+          titre={
+            <img
+              src={titre}
+              onMouseDown={(e) => e.preventDefault()}
+            />
+          }
+        />
+      </Suspense>
     </AppBar>
   );
 };
